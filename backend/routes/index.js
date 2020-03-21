984ea17f4ac7a9fed2d493dc3643f53690c894f1
Ryan Doyle <ryan@Ryans-iMac.local>
2020-03-21 14:52:07 -0700

    wipe backend

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => res.send('Hello World!'));
-
-router.get('/test', (req, res) => res.send({ testing: "hello!" }));
-
-module.exports = router;
