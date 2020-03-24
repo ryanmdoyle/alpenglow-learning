e19aa9b7de8b74d3387c4018b5d42013f9956a7b
Ryan Doyle <ryan@Ryans-iMac.local>
2020-03-24 10:57:54 -0700

    server beginnings

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
 import Login from '../components/Login';
 
 const HomePage = () => {
+  const testGet = async () => {
+    console.log('testGet running')
+
+    axios.get('http://localhost:4000/get')
+      .then((res) => {
+        console.log(res);
+      })
+  }
+
   return (
     <div>
       <h2>Welcome to Next.js!</h2>
       <Login />
+      <button onClick={() => { testGet() }}>Test Query</button>
     </div>
   )
 
