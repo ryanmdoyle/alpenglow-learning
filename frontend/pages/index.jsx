b5004eed5f3d32a53350fcb37d3971c411d31625
Ryan Doyle <ryan@Ryans-iMac.local>
2020-05-15 14:15:22 -0700

    remove standalone current user query

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -5,28 +5,15 @@ import { css } from '@emotion/core';
 
 import Loading from '../components/Loading';
 
-const GET_CURRENT_USER = gql`
-  query GET_CURRENT_USER {
-    getCurrentUser {
-      googleId
-      firstName
-    }
-  }
-`;
+const HomePage = ({ currentUser }) => {
 
-const HomePage = () => {
-  const { loading, error, data } = useQuery(GET_CURRENT_USER);
-
-  if (error) return null;
-  if (loading) return <Loading />
   return (
     <div css={css`padding: 0 1rem;`}>
       <h2>Welcome to Alpenglow!</h2>
-      {data.getCurrentUser ? <h1>{data.getCurrentUser.firstName}</h1> : null}
+      {currentUser ? <h1>{currentUser.firstName}</h1> : null}
     </div>
   )
 
 }
 
 export default HomePage;
-export { GET_CURRENT_USER };
