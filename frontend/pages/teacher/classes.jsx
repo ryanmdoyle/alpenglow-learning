fa835f97ac5b3b65a77ddbb71b7f9032fc08a7d5
Ryan Doyle <ryan@Ryans-iMac.local>
2020-05-15 18:45:27 -0700

    fixed classes return type in courses

--- frontend/pages/teacher/classes.jsx
+++ frontend/pages/teacher/classes.jsx
@@ -6,12 +6,16 @@ import { useQuery } from '@apollo/react-hooks'
 import Loading from '../../components/Loading';
 import PagePadding from '../../components/styled/PagePadding';
 import CreateClass from '../../components/CreateClass';
+import PlusButton from '../../components/styled/elements/PlusButton';
 
 const INSTRUCTING_COURSES_QUERY = gql`
   query INSTRUCTING_COURSES_QUERY {
     getInstructingCourses {
       _id
       name
+      classes {
+        
+      }
     }
   }
 `;
@@ -21,11 +25,16 @@ const teacherClasses = () => {
 
   if (error) return null;
   if (loading) return <Loading />
+  console.log(data.getInstructingCourses)
   return (
     <div>
       <PageTitle title='Your Classes' />
       <PagePadding>
-        {data.getInstructingCourses && data.getInstructingCourses.map(course => (
+        {data?.getInstructingCourses?.length == 0 && (
+          <PlusButton />
+        )}
+
+        {data?.getInstructingCourses?.map(course => (
           <h4 key={course._id}>{course.name}</h4>
         ))}
         <h3>Create Class</h3>
