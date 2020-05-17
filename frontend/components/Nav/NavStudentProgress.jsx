73c229d226e20e731bf6b42e4748984eae22839b
Ryan Doyle <ryan@Ryans-iMac.local>
2020-05-17 11:48:50 -0700

    add query for list of classes in nav

--- frontend/components/Nav/NavStudentProgress.jsx
+++ frontend/components/Nav/NavStudentProgress.jsx
@@ -8,6 +8,8 @@ import NavSectionHeader from '../styled/NavSectionHeader';
 import NavItem from './NavItem';
 import Loading from '../Loading';
 
+const innerPadding = css`padding-left: 1rem;`;
+
 const INSTRUCTING_CLASSES_QUERY = gql`
   query INSTRUCTING_CLASSES_QUERY {
     getInstructingClasses {
@@ -25,13 +27,12 @@ const NavStudentProgress = () => {
     <NavSectionPadding>
       <NavSectionHeader title='Student Progress' />
       <ul css={css`list-style:none;margin:1rem 0.2rem;padding:0;`}>
-        <NavItem title='All Your Students' href='/teacher/classes' />
-        <div css={css`padding-left: 1rem;`}>
-          <NavItem title='placeholder class' href='/teacher/allStudents' />
-        </div>
-        <div css={css`padding-left: 1rem;`}>
-          <NavItem title='placeholder class' href='/teacher/allStudents' />
-        </div>
+        <NavItem title='All Your Students' href='/teacher/classes' css={css`padding-left: 2rem;`} />
+        {data?.getInstructingClasses?.map(c => (
+          <div css={innerPadding}>
+            <NavItem title={c.name} href='/teacher/allStudents' />
+          </div>
+        ))}
         <NavItem title='Grading' href='/teacher/grading' />
       </ul>
     </NavSectionPadding>
