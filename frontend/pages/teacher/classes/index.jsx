6fb65557ab3c7dd2e0ebea590bab0a1d33ea2a62
Ryan Doyle <doylecodes@gmail.com>
2020-05-22 15:18:11 -0700

    replace page modal with context modal

--- frontend/pages/teacher/classes/index.jsx
+++ frontend/pages/teacher/classes/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { css } from '@emotion/core';
 import PageTitle from '../../../components/PageTitle';
 import { gql } from 'apollo-boost';
@@ -11,6 +11,7 @@ import PlusButton from '../../../components/styled/elements/PlusButton';
 import TextButton from '../../../components/styled/elements/TextButton';
 import Modal from '../../../components/styled/Modal';
 import ClassTable from '../../../components/ClassInfo';
+import ModalContext from '../../../components/context/ModalContext';
 
 const INSTRUCTING_COURSES_QUERY = gql`
   query INSTRUCTING_COURSES_QUERY {
@@ -31,8 +32,7 @@ const INSTRUCTING_COURSES_QUERY = gql`
 // On  this page, show a list of courses, with a button after to add a "class"
 const teacherClasses = () => {
   const { loading, error, data } = useQuery(INSTRUCTING_COURSES_QUERY);
-  const [newClassModal, toggleNewClassModal] = useState(false);
-  const [modalId, setModalId] = useState(null);
+  const modal = useContext(ModalContext);
 
   if (error) return null;
   if (loading) return <Loading />
@@ -49,8 +49,8 @@ const teacherClasses = () => {
             <ClassTable classData={course.classes} />
             <div css={css`display: flex; justify-content: flex-end;`}>
             <TextButton text={`Add class to ${course.name}`} whenClicked={() => {
-                  setModalId(course._id);
-                  toggleNewClassModal(!newClassModal);
+                  modal.setChildComponent(<CreateClass courseId={course._id} />);
+                  modal.setIsOpen(true);
                 }}></TextButton>
 
             </div>
@@ -58,13 +58,6 @@ const teacherClasses = () => {
         ))}
 
       </PagePadding>
-
-      {newClassModal && (
-        <Modal isOpen={newClassModal} toggle={toggleNewClassModal}>
-          <CreateClass courseId={modalId} />
-        </Modal>
-      )}
-
     </div >
   );
 };
