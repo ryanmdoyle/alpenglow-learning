d4e56174df45153eaaf35265d6d66ea771d53c52
Ryan Doyle <doylecodes@gmail.com>
2020-05-22 12:28:58 -0700

    removed User require

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const User = require('./models/User');
-
 const db = mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
