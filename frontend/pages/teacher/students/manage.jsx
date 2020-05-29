c8636e20d4ccd5b25b1d805702f7b6d18a3937ea
Ryan Doyle <doylecodes@gmail.com>
2020-05-29 14:43:14 -0700

    add query for students

--- frontend/pages/teacher/students/manage.jsx
+++ frontend/pages/teacher/students/manage.jsx
@@ -5,7 +5,18 @@ import { useQuery } from '@apollo/react-hooks'
 
 import PagePadding from '../../../components/styled/PagePadding';
 
+const GET_INSTRUCTING_STUDENTS_QUERY = gql`
+  query GET_INSTRUCTING_STUDENTS_QUERY {
+    getInstructingStudents {
+      _id
+      name
+    }
+  }
+`;
+
 const manage = () => {
+  const { loading, error, data } = useQuery(GET_INSTRUCTING_STUDENTS_QUERY);
+  console.log(data);
   return (
     <>
       <PageTitle>Manage Students</PageTitle>
