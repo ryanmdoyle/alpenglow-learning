5fe4c59dc702de09005e06791b6de87ec447cf98
Ryan Doyle <doylecodes@gmail.com>
2020-05-30 18:03:00 -0700

    added subtitle prop

--- frontend/components/PageTitle.jsx
+++ frontend/components/PageTitle.jsx
@@ -6,24 +6,32 @@ import { css } from '@emotion/core';
 const titleStyles = css`
   width: 100%;
   min-height: 75px;
-  padding: 0 2rem;
+  padding: 1.2rem 2rem;
   background-color: var(--blueLight);
   h3 {
     margin: 0;
-    padding: 1.2rem 0;
+    padding: 0rem 0;
+  }
+  span {
+    color: var(--blueMedium);
   }
 `;
 
-const PageTitle = ({ children }) => {
+const PageTitle = ({ children, subtitle }) => {
+  const capitalizedSubtitle = subtitle[0] + subtitle.slice(1).toLowerCase();
   return (
     <div css={titleStyles}>
       <h3>{children}</h3>
+      {subtitle &&
+        <span>{capitalizedSubtitle} Playlist</span>
+      }
     </div>
   );
 };
 
 PageTitle.propTypes = {
   children: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
 }
 
 export default PageTitle;
