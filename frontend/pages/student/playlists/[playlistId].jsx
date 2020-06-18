1eec164bec7fea7e19d14b923443c1861f6ecd78
Ryan Doyle <doylecodes@gmail.com>
2020-06-18 10:32:32 -0700

    fix import route

--- frontend/pages/student/playlists/[playlistId].jsx
+++ frontend/pages/student/playlists/[playlistId].jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router'
 
-import Playlist from '../../../../components/playlist/Playlist';
+import Playlist from '../../../components/playlist/Playlist';
 
 const studentPlaylistId = () => {
   const router = useRouter();
