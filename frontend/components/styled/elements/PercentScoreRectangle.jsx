f3a11a824c2194a6577dcbb2938305385742344e
Ryan Doyle <doylecodes@gmail.com>
2020-07-15 20:40:14 -0700

    move styled percent box to component

---
+++ frontend/components/styled/elements/PercentScoreRectangle.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { css } from '@emotion/core';
+
+const scoreStyle = css`
+  width: 3rem;
+  max-height: 1.7rem;
+  background-color: var(--green);
+  border-radius: 2px;
+  padding: 0 0.5rem;
+  text-align: center;
+`;
+
+const scoreColors = (score) => {
+  if (score >= 80) {
+    return css`
+      background-color: var(--green);
+    `
+  }
+
+  if (score >= 70) {
+    return css`
+      background-color: var(--yellow);
+    `
+  }
+
+  if (score !== null && score >= 0) {
+    return css`
+      background-color: var(--red);
+    `
+  }
+  return null
+};
+
+const PercentScoreRectangle = ({ percent }) => {
+  const scoreColorCoded = scoreColors(percent);
+
+  return (
+    <div css={[scoreStyle, scoreColorCoded]}>{percent}%</div>
+  );
+};
+
+export default PercentScoreRectangle;
