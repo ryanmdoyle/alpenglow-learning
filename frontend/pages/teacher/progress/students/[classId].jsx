235a191f78bf4539650d129f8abaae60028fd68f
Ryan Doyle <doylecodes@gmail.com>
2020-07-16 17:24:32 -0700

    get class title for the page

--- frontend/pages/teacher/progress/students/[classId].jsx
+++ frontend/pages/teacher/progress/students/[classId].jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { css } from '@emotion/core';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import { useRouter } from 'next/router';
 
 import PageTitle from '../../../../components/PageTitle';
 import PagePadding from '../../../../components/styled/PagePadding';
 import ClassProgressTable from '../../../../components/progress/classProgress/ClassProgressTable';
 
+const GET_CLASS_TITLE = gql`
+  query GET_CLASS_TITLE($classId: ID!) {
+    getInstructingClass(classId: $classId) {
+      _id
+      name
+    }
+  }
+`;
+
 const classProgress = () => {
+  const { query: { classId } } = useRouter();
+  const { data } = useQuery(GET_CLASS_TITLE, {
+    variables: { classId },
+  })
+  const name = data?.getInstructingClass?.name;
   return (
     <div>
-      <PageTitle>The Class</PageTitle>
+      <PageTitle>Student Progress{name ? ` - ${name}` : null}</PageTitle>
       <PagePadding>
         <ClassProgressTable />
       </PagePadding>
