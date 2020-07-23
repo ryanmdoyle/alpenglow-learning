8dd4f85bfbdaa5a1efcab26af346b0bdd4658b2f
Ryan Doyle <doylecodes@gmail.com>
2020-07-23 16:08:18 -0700

    add process.env.port for server listen

--- backend/index.js
+++ backend/index.js
@@ -66,7 +66,12 @@ server.applyMiddleware({
 server.installSubscriptionHandlers(httpServer);
 
 
-httpServer.listen({ port: 4000 }, () => {
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-  console.log(`🚀 Subscriptions ready at ws://localhost:4000${server.subscriptionsPath}`)
+httpServer.listen({ port: process.env.PORT || 4000 }, () => {
+  if (process.env.PORT) {
+    console.log(`🚀 Server ready at ${process.env.PORT}${server.graphqlPath}`)
+    console.log(`🚀 Subscriptions ready at ${process.env.PORT}${server.subscriptionsPath}`)
+  } else {
+    console.log(`🚀 Server (Dev) ready at http://localhost:4000${server.graphqlPath}`)
+    console.log(`🚀 Subscriptions (Dev) ready at ws://localhost:4000${server.subscriptionsPath}`)
+  }
 })
