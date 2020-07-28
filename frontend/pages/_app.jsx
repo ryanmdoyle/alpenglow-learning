3a15400939dd22ceaf05b36ee270d3e3e2cfb04d
Ryan Doyle <doylecodes@gmail.com>
2020-07-28 11:03:20 -0700

    remove old css, add favicon

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -14,37 +14,6 @@ import { UserProvider } from '../components/context/UserContext';
 import { AlertProvider } from '../components/context/AlertContext';
 import { ModalProvider } from '../components/context/ModalContext';
 
-const layout = css`
-  height: 100vh;
-  max-width: 100vw;
-  display: grid;
-  grid-template-columns: var(--navWidth) 1fr;
-  grid-template-rows: 1fr;
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
-  grid-template-areas:
-    "nav main";
-  overflow: hidden;
-  scrollbar-width: thin;
-  scrollbar-color: var(--scrollbar) var(--scrollbarBG);
-
-    nav {
-      grid-area: nav;
-      border-right: 1px solid var(--grey);
-      height: 100vh;
-      box-shadow: var(--shadowMedium);
-      background-color: var(--blueDark);
-    }
-
-    main {
-      position: relative;
-      grid-area: main;
-      height: 100vh;
-      width: 100%;
-      overflow-y: scroll;
-    }
-`
-
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
@@ -53,7 +22,7 @@ function MyApp({ Component, pageProps }) {
           <ModalProvider>
             <Global styles={global} />
             <Head>
-              {/* <script src="https://apis.google.com/js/platform.js" async defer></script> */}
+              <link rel="icon" href="/alpenglow.svg"></link>
             </Head>
             <LayoutController Component={Component} pageProps={pageProps} />
           </ModalProvider>
