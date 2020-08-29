be56a55884f90550e0378f0bc5a07be623bb7d76
Ryan Doyle <doylecodes@gmail.com>
2020-08-29 15:28:24 -0700

    reroute from playlist if not in course enrolled

--- frontend/pages/student/playlists/[playlistId].jsx
+++ frontend/pages/student/playlists/[playlistId].jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router'
+import { useQuery } from '@apollo/client';
 
 import PageFade from '../../../components/styled/blocks/PageFade';
 import Playlist from '../../../components/playlist/Playlist';
+import Loading from '../../../components/Loading';
+import { GET_ENROLLED_COURSES } from '../../../gql/queries';
 
 const studentPlaylistId = () => {
   const router = useRouter();
   const { playlistId } = router.query;
 
+  const { data, loading, error } = useQuery(GET_ENROLLED_COURSES);
+  const ess = data?.getCoursesEnrolled?.map(course => course.essentialPlaylists.map(playlist => playlist._id))
+  const core = data?.getCoursesEnrolled?.map(course => course.corePlaylists.map(playlist => playlist._id))
+  const chall = data?.getCoursesEnrolled?.map(course => course.challengePlaylists.map(playlist => playlist._id))
+  const playlists = data?.getCoursesEnrolled ? [...ess, ...core, ...chall].flat() : null;
+
+  if (loading) return <Loading />
+  if (!playlists.includes(playlistId)) {
+    router.push('/student')
+  }
   return (
     <PageFade>
       <Head>
--- frontend/pages/teacher/manage/playlists/[playlistId].jsx
+++ frontend/pages/teacher/manage/playlists/[playlistId].jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import { useQuery } from '@apollo/client';
 
 import PageFade from '../../../../components/styled/blocks/PageFade';
 import Playlist from '../../../../components/playlist/Playlist';
+import Loading from '../../../../components/Loading';
+import { GET_INSTRUCTING_COURSES } from '../../../../gql/queries';
 
 const teacherPlaylistId = () => {
   const router = useRouter();
   const { playlistId } = router.query;
 
+  const { data, loading, error } = useQuery(GET_INSTRUCTING_COURSES);
+  const ess = data?.getCoursesInstructing.map(course => course.essentialPlaylists.map(playlist => playlist._id))
+  const core = data?.getCoursesInstructing.map(course => course.corePlaylists.map(playlist => playlist._id))
+  const chall = data?.getCoursesInstructing.map(course => course.challengePlaylists.map(playlist => playlist._id))
+  const playlists = data?.getCoursesInstructing ? [...ess, ...core, ...chall].flat() : null;
+
+  if (loading) return <Loading />
+  if (!playlists.includes(playlistId)) {
+    router.push('/teacher')
+  }
   return (
     <PageFade>
       <Head>
